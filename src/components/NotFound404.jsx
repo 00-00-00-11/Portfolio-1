2f6a37027c9b653ad76ceb66fb3b16c4e9f7146f
ThatGuyJamal <jamallyons8@gmail.com>
2021-02-13 01:05:34 -0500

    Update NotFound404.jsx

--- src/components/NotFound404.jsx
+++ src/components/NotFound404.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "./css/util/Main.css";
 const NotFound404Error = () => {
 	return (
 		<div className="page-build">
 			<h1>Sorry!</h1>
+			<img
+				className="test-img-main-def"
+				src="https://media.giphy.com/media/14uQ3cOFteDaU/source.gif"
+				alt="Gamer Gif"
+			></img>
 			<p>This page can't be found!</p>
 			<Link to="/">Back to home page...</Link>
 		</div>
