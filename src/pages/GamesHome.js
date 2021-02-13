f521ebbffde1f24c9555b07f610f774398e7fd9a
ThatGuyJamal <jamallyons8@gmail.com>
2021-02-12 23:33:00 -0500

    Update GamesHome.js

--- src/pages/GamesHome.js
+++ src/pages/GamesHome.js
@@ -1,9 +1,20 @@
 import React from "react";
+import "../build/App.css";
+import { Link } from "react-router-dom";
 
 function GamesHome() {
 	return (
 		<div>
-			<h1>Nothing Yet!</h1>
+			<div className="App">
+				<header className="App-header">
+					<h1>Games!</h1>
+					<p>
+						Each button below goes to a different game to play. If there are any
+						bugs or errors repor them to the{" "}
+						<Link to="/contact-us">contact page.</Link> Thanks and enjoy!
+					</p>
+				</header>
+			</div>
 		</div>
 	);
 }
